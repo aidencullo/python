ae7816a1fd0e3f2ca003d0d8b7e419a9637c04c5
Aiden Cullo <aidencullo@gmail.com>
2025-09-23 10:04:21 -0400

    Apply changes

--- filter.py
+++ filter.py
@@ -1,9 +1,14 @@
 # Given a list of strings, use filter to create a new list containing only the strings that start with the letter "a".
 
+from functools import partial
 
+def starts_with(s, prefix):
+    return s.startswith(prefix)
 
 def filter_strs(strs):
-    return list(filter(lambda s: s[0] == 'a', strs))
+    starts_with_a = partial(starts_with, prefix='a')
+    lst = list(filter(starts_with_a, strs))
+    return lst
 
 
 # Tests
