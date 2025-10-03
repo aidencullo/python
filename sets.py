c11ba509920dd833a35763b402295bee7396b551
Aiden Cullo <aidencullo@gmail.com>
2025-10-03 17:46:59 -0400

    Refactor sets.py to use a function

--- sets.py
+++ sets.py
@@ -3,54 +3,52 @@ B = set(range(10))
 empty_set = set()
 
 
+def test_set_properties(A, B):
+        # difference
+    A_B = A - B
+    B_A = B - A
+    assert A.difference(B) == A_B
+    assert A_B == empty_set
+    assert not A_B
+    assert A_B <= B
+    assert A_B <= A
+    assert B_A <= B
+    assert not (A <= B_A)
 
-# difference
-A_B = A - B
-B_A = B - A
-assert A.difference(B) == A_B
-assert A_B == empty_set
-assert not A_B
-assert A_B <= B
-assert A_B <= A
-assert B_A <= B
-assert not (A <= B_A)
+    # subset
+    assert A <= B
+    assert A <= A
+    assert A < B
+    assert not (A < A)
+    assert not (B <= A)
+    assert not (B < A)
+    assert A.issubset(B)
+    assert A.issubset(A)
+    assert not B.issubset(A)
 
-# subset
-assert A <= B
-assert A <= A
-assert A < B
-assert not (A < A)
-assert not (B <= A)
-assert not (B < A)
-assert A.issubset(B)
-assert A.issubset(A)
-assert not B.issubset(A)
+    # Intersection
+    assert A & A == A
+    assert A & A != B
+    assert A & B == B & A
+    assert A & B <= A
+    assert A & B <= B
+    assert A & B <= A | B
+    assert A & B == A
 
-# Intersection
-assert A & A == A
-assert A & A != B
-assert A & B == B & A
-assert A & B <= A
-assert A & B <= B
-assert A & B <= A | B
-assert A & B == A
+    # union
+    assert A | A == A
+    assert A | B >= B
+    assert A | B >= A
+    assert A | B >= A & B
+    assert A | B == B
+    assert B | B == B
 
-# union
-assert A | A == A
-assert A | B >= B
-assert A | B >= A
-assert A | B >= A & B
-assert A | B == B
-assert B | B == B
+    # symmetric diff
+    assert A ^ B <= B
+    assert A ^ B == B ^ A
+    assert A ^ B == (A | B) - (A & B)
+    assert (A ^ B) & (A & B) == set()
 
-# symmetric diff
-assert A ^ B <= B
-assert A ^ B == B ^ A
-assert A ^ B == (A | B) - (A & B)
-assert (A ^ B) & (A & B) == set()
+    print("all set tests passed!")
 
-
-# xor?
-
-# empty
-
+test_set_properties()
