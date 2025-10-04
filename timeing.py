fcaef97a821b06948d54060ea64b3d5bbe8b0011
Aiden Cullo <aidencullo@gmail.com>
2025-10-04 14:49:58 -0400

    rudimentary timing module
    I'm sure this already exists, but I'd like to do it for my own
    knowledge tbh

---
+++ timeing.py
@@ -0,0 +1,22 @@
+import time
+
+def quadratic(n):
+    x = 0
+
+    for i in range(n):
+        for j in range(i, n):
+            x += 1
+
+def timed_quadratic(n):
+    t0 = time.time()
+    quadratic(n)
+    t1 = time.time()
+    return t1-t0
+
+def timed_quadratic_suite():
+    for i in range(10):
+        factor = 1000
+        t = timed_quadratic(i * factor)
+        print(t, i * factor)
+
+timed_quadratic_suite()
