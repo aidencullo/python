3ea802c1cf60f7fb4cc54a5d59c651c181358e95
Aiden Cullo <aidencullo@gmail.com>
2025-10-08 14:51:27 -0400

    more fun w  reduce and random

--- sorting.py
+++ sorting.py
@@ -8,3 +8,18 @@ counter_list = list(counter)
 sorted_tuples = sorted(counter.items(), key=lambda key_value: key_value[1])
 sorted_tuples = sorted(d.items())
 print(sorted_tuples)
+
+from functools import reduce
+import operator
+import random
+
+[random.randint(1, 100) for i in range(100)]
+
+
+
+def min_from_reduce():
+    randoms = random.choices(range(100), k=10)
+    print(randoms)
+    return reduce(lambda x, y: x if x < y else y, randoms)
+
+min_from_reduce()
